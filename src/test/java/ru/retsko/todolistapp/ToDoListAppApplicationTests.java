db8756c0892066955499e75830e5f0d6a5c8dfed
rubikkubika <artw123@mail.ru>
2024-11-24 15:35:14 +0300

    Start

---
+++ src/test/java/ru/retsko/todolistapp/ToDoListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.retsko.todolistapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
